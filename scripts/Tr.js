5c49a413b5986f8030b47aa416f02754b44a891c
Tarekfaisal7M <faisal71106@gmail.comgit config --global user.name sajibgit config --global user.name Tarekfaisal7M git config --global user.email faisal71106@gmail.com>
2024-02-13 02:53:04 +0600

    added triangle js value

--- scripts/Tr.js
+++ scripts/Tr.js
@@ -1,3 +1,13 @@
+// triangle base value
 function triangleCalculateArea(){
-    console.log(`i love you js`);
+   const triangleBasedInput = document.getElementById(`triangle-base`);
+   const triangleBasedText = triangleBasedInput.value;
+   const base = parseFloat(triangleBasedText);
+   console.log(base);
+//    triangle.height value
+   const triangleInput =document.getElementById(`triangle-height`);
+   const triangleText = triangleInput.value;
+   const height = parseFloat(triangleText);
+   console.log(height);
 }
+// triangle height value
