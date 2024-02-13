3c424830f14263d8ab48294e721a0611f77608ee
Tarekfaisal7M <faisal71106@gmail.comgit config --global user.name sajibgit config --global user.name Tarekfaisal7M git config --global user.email faisal71106@gmail.com>
2024-02-13 19:27:08 +0600

    added new section for card

--- scripts/Tr.js
+++ scripts/Tr.js
@@ -4,10 +4,19 @@ function triangleCalculateArea(){
    const triangleBasedText = triangleBasedInput.value;
    const base = parseFloat(triangleBasedText);
    console.log(base);
+
 //    triangle.height value
    const triangleInput =document.getElementById(`triangle-height`);
    const triangleText = triangleInput.value;
    const height = parseFloat(triangleText);
    console.log(height);
+   
+   // calculate triangle area
+   const area = .5 * base * height;
+   console.log(`area is triangle of:`, area);
+
+   // display triangle area
+   const triangleAraSpan = document.getElementById(`triangle-area`);
+   triangleAraSpan.innerText = area;
 }
-// triangle height value
+
